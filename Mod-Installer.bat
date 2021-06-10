1dba597bf73f9c7aa7b9da727f7d6f407ee69c5a
2020Sanoj <75573284+2020Sanoj@users.noreply.github.com>
2021-06-10 18:00:55 +0200

    Add files via upload

--- Mod-Installer.bat
+++ Mod-Installer.bat
@@ -7,7 +7,7 @@ endlocal
 
 	ECHO Java ist nicht installiert, der Mod-Installer wird nun gestoppt, 
 	ECHO bitte installiere Java um diesen Mod-Installer nutzten zu k”nnen! 
-	ECHO https://adoptopenjdk.net/?variant=openjdk16&jvmVariant=hotspot
+	ECHO https://adoptopenjdk.net/
 	ECHO.
     ECHO 1. Update/Install Mod-Installer - Empfohlen
     ECHO 2. Update/Install Dev-Mod-Installer - evt. Fehlerhaft
@@ -32,36 +32,38 @@ CLS
 	ECHO.
 	ECHO Client-Mods/Modpack Versionen:
 	ECHO.
-	ECHO 1. 1.16.5
+	ECHO 1. 1.16
+	ECHO 2. 1.17
 	ECHO.
-	ECHO 2. Texture Packs
+	ECHO 3. Texture Packs
 	ECHO.
-	ECHO 3. Backups/Modprofile
+	ECHO 4. Backups/Modprofile
 	ECHO.
-	ECHO 4. Minecraft-Launcher starten
+	ECHO 5. Minecraft-Launcher starten
 	ECHO.
 	ECHO Funktionen:
 	ECHO.
-	ECHO 5. Update/Install Mod-Installer - Empfohlen
-	ECHO 6. Update/Install Dev-Mod-Installer - evt. Fehlerhaft
-    ECHO 7. Installer neustarten.
-	ECHO 8. Installer beenden.
-	ECHO 9. Feedback hinterlassen
+	ECHO 6. Update/Install Mod-Installer - Empfohlen
+	ECHO 7. Update/Install Dev-Mod-Installer - evt. Fehlerhaft
+    ECHO 8. Installer neustarten.
+	ECHO 9. Installer beenden.
+	ECHO 10. Feedback hinterlassen
 	ECHO.
     CHOICE /C 123456789 /M "Auswahl: "
 
     :: Note - list ERRORLEVELS in decreasing order
-    IF ERRORLEVEL 8 GOTO fb
-    IF ERRORLEVEL 8 GOTO end
-    IF ERRORLEVEL 7 GOTO start
-    IF ERRORLEVEL 6 GOTO Dev
-    IF ERRORLEVEL 5 GOTO update
-    IF ERRORLEVEL 4 GOTO mcl
-    IF ERRORLEVEL 3 GOTO bp
-	IF ERRORLEVEL 2 GOTO tp
-    IF ERRORLEVEL 1 GOTO 1.16.5
+    IF ERRORLEVEL 10 GOTO fb
+    IF ERRORLEVEL 9 GOTO end
+    IF ERRORLEVEL 8 GOTO start
+    IF ERRORLEVEL 7 GOTO Dev
+    IF ERRORLEVEL 6 GOTO update
+    IF ERRORLEVEL 5 GOTO mcl
+    IF ERRORLEVEL 4 GOTO bp
+	IF ERRORLEVEL 3 GOTO tp
+	IF ERRORLEVEL 2 GOTO 1.17
+    IF ERRORLEVEL 1 GOTO 1.16
 
-:1.16.5
+:1.17
 CLS
 ECHO Client: FPS = Sodium, Shader = Optifine
 ECHO Only = Nur Grafikmods, Lite = Wenige leichte Mods, Full = Minimap, Hwyla, usw.
@@ -69,32 +71,115 @@ ECHO Client = Auf Vanila Servern spielbar, Modpack = Nur auf Servern mit dem sel
 ECHO.
 ECHO Only Optifine/Modloader:
 ECHO.
-ECHO A. 1.16.5 Only Optifine		- https://optifine.net/home
-ECHO B. 1.16.5 Only Fabric-Loader	- https://fabricmc.net/
-ECHO C. 1.16.5 Only Forge-Loader	- https://files.minecraftforge.net/net/minecraftforge/forge/
+ECHO 1. 1.17 Only Optifine		- https://optifine.net/downloads/
+ECHO 2. 1.17 Only Fabric-Loader	- https://fabricmc.net/use/
+ECHO 3. 1.17 Only Forge-Loader	- https://files.minecraftforge.net/net/minecraftforge/forge/
+ECHO.
+ECHO 4. Installer neustarten.
+ECHO 5. Installer beenden.
+ECHO.
+ECHO Stelle sicher, dass du im Minecraft Launcher unten Links den richtigen Modloader ausw„hlst!
+ECHO.
+
+CHOICE /C 12345 /M "EMPFEHLUNG! SICHERE MODS/CONFIGS IN EINEM PROFIL! SIE WERDEN ENTFERNT! Auswahl: "
+IF ERRORLEVEL 5 GOTO end
+IF ERRORLEVEL 4 GOTO start
+IF ERRORLEVEL 3 GOTO start
+IF ERRORLEVEL 2 GOTO 1.17-fabric
+IF ERRORLEVEL 1 GOTO 1.17-optifine
+
+
+:1.17-optifine
+echo Instalation startet...
+C:
+cd %userprofile%\AppData\Roaming\.minecraft
+tar cf Backup.tar mods config
+curl --output 1.17-optifine.jar --url https://download.san0j.de/mods/1.17-optifine.jar
+echo Im n„chsten Schritt ”ffnet sich automatisch ein Fenster klicke dort nur auf "Install"!
+Pause
+java -jar 1.17-optifine.jar
+del /S /Q 1.17-optifine.jar
+echo Fertig!
+
+ECHO 1. Mod-Installer neustarten.
+ECHO 2. Installer beenden.
+CHOICE /C 12 /M "Auswahl: "
+:: Note - list ERRORLEVELS in decreasing order
+IF ERRORLEVEL 2 GOTO end
+IF ERRORLEVEL 1 GOTO start
+
+:1.17-fabric
+echo Instalation startet...
+C:
+cd %userprofile%\AppData\Roaming\.minecraft
+tar cf Backup.tar mods config
+curl --output fabric.jar --url https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.7.3/fabric-installer-0.7.3.jar
+java -jar fabric.jar client -mcversion 1.17
+del /S /Q fabric.jar
+echo Fertig!
+
+ECHO 1. Mod-Installer neustarten.
+ECHO 2. Installer beenden.
+CHOICE /C 12 /M "Auswahl: "
+:: Note - list ERRORLEVELS in decreasing order
+IF ERRORLEVEL 2 GOTO end
+IF ERRORLEVEL 1 GOTO start
+
+:1.17-forge
+echo Instalation startet...
+C:
+cd %userprofile%\AppData\Roaming\.minecraft
+tar cf Backup.tar mods config
+curl --output forge.jar --url https://maven.minecraftforge.net/net/minecraftforge/forge/link
+echo Im n„chsten Schritt ”ffnet sich automatisch ein Fenster klicke dort nur auf "OK"!
+Pause
+java -jar forge.jar
+del /S /Q forge.jar
+del /S /Q forge.jar.log
+echo Fertig! 
+
+ECHO 1. Mod-Installer neustarten.
+ECHO 2. Installer beenden.
+CHOICE /C 12 /M "Auswahl: "
+:: Note - list ERRORLEVELS in decreasing order
+IF ERRORLEVEL 2 GOTO end
+IF ERRORLEVEL 1 GOTO start
+
+
+:1.16
+CLS
+ECHO Client: FPS = Sodium, Shader = Optifine
+ECHO Only = Nur Grafikmods, Lite = Wenige leichte Mods, Full = Minimap, Hwyla, usw.
+ECHO Client = Auf Vanila Servern spielbar, Modpack = Nur auf Servern mit dem selben installierten Modpack spielbar und im Singleplayer.
+ECHO.
+ECHO Only Optifine/Modloader:
+ECHO.
+ECHO A. 1.16 Only Optifine		- https://optifine.net/downloads/
+ECHO B. 1.16 Only Fabric-Loader	- https://fabricmc.net/use/
+ECHO C. 1.16 Only Forge-Loader	- https://files.minecraftforge.net/net/minecraftforge/forge/
 ECHO.
 ECHO Client-Modpacks:
 ECHO.
-ECHO D. 1.16.5 Client Only FPS
-ECHO E. 1.16.5 Client Mods Lite FPS
-ECHO F. 1.16.5 Client Mods Full FPS
+ECHO D. 1.16 Client Only FPS
+ECHO E. 1.16 Client Mods Lite FPS
+ECHO F. 1.16 Client Mods Full FPS
 ECHO.
-ECHO G. 1.16.5 Client Only Shader
-ECHO H. 1.16.5 Client Mods Lite Shader
-ECHO I. 1.16.5 Client Mods Full Shader
+ECHO G. 1.16 Client Only Shader
+ECHO H. 1.16 Client Mods Lite Shader
+ECHO I. 1.16 Client Mods Full Shader
 ECHO.
 ECHO Modpacks:
 ECHO.
-ECHO J. 1.16.5 Sanoj's Forge Modpack Lite 	 	- https://www.curseforge.com/minecraft/modpacks/sanojs-forge-loader-modpack/
-ECHO K. 1.16.5 Sanoj's Forge Modpack Full 	 	- https://www.curseforge.com/minecraft/modpacks/sanojs-forge-loader-modpack/
+ECHO J. 1.16 Sanoj's Forge Modpack Lite 	 	- https://www.curseforge.com/minecraft/modpacks/sanojs-forge-loader-modpack/
+ECHO K. 1.16 Sanoj's Forge Modpack Full 	 	- https://www.curseforge.com/minecraft/modpacks/sanojs-forge-loader-modpack/
 ECHO.
-ECHO L. 1.16.5 Sanoj's Fabric Modpack Lite 		- https://www.curseforge.com/minecraft/modpacks/sanojs-fabric-loader-modpack
-ECHO M. 1.16.5 Sanoj's Fabric Modpack Full  	- https://www.curseforge.com/minecraft/modpacks/sanojs-fabric-loader-modpack
+ECHO L. 1.16 Sanoj's Fabric Modpack Lite 		- https://www.curseforge.com/minecraft/modpacks/sanojs-fabric-loader-modpack
+ECHO M. 1.16 Sanoj's Fabric Modpack Full  		- https://www.curseforge.com/minecraft/modpacks/sanojs-fabric-loader-modpack
 ECHO.
 ECHO Server Modpacks:
 ECHO.
-ECHO N. 1.16.5 Forge Server Pack
-ECHO O. 1.16.5 Fabric Server Pack
+ECHO N. 1.16 Forge Server Pack
+ECHO O. 1.16 Fabric Server Pack
 ECHO.
 ECHO P. Installer neustarten.
 ECHO Q. Installer beenden.
@@ -105,21 +190,21 @@ ECHO.
 CHOICE /C abcdefghijklmnopq /M "EMPFEHLUNG! SICHERE MODS/CONFIGS IN EINEM PROFIL! SIE WERDEN ENTFERNT! Auswahl: "
 IF ERRORLEVEL 17 GOTO end
 IF ERRORLEVEL 16 GOTO start
-IF ERRORLEVEL 15 GOTO 1.16.5-fas
-IF ERRORLEVEL 14 GOTO 1.16.5-fos
-IF ERRORLEVEL 13 GOTO 1.16.5-sanoj's-fabric-modpack-full
-IF ERRORLEVEL 12 GOTO 1.16.5-sanoj's-fabric-modpack-lite
-IF ERRORLEVEL 11 GOTO 1.16.5-sanoj's-forge-modpack-full
-IF ERRORLEVEL 10 GOTO 1.16.5-sanoj's-forge-modpack-lite
-IF ERRORLEVEL 9 GOTO 1.16.5-Client-Mods-Full-Shader
-IF ERRORLEVEL 8 GOTO 1.16.5-Client-Mods-Lite-Shader
-IF ERRORLEVEL 7 GOTO 1.16.5-Client-Only-Shader
-IF ERRORLEVEL 6 GOTO 1.16.5-Client-Mods-Full-FPS
-IF ERRORLEVEL 5 GOTO 1.16.5-Client-Mods-Lite-FPS
-IF ERRORLEVEL 4 GOTO 1.16.5-Client-Only-FPS
-IF ERRORLEVEL 3 GOTO 1.16.5-forge
-IF ERRORLEVEL 2 GOTO 1.16.5-fabric
-IF ERRORLEVEL 1 GOTO 1.16.5-optifine
+IF ERRORLEVEL 15 GOTO 1.16-fas
+IF ERRORLEVEL 14 GOTO 1.16-fos
+IF ERRORLEVEL 13 GOTO 1.16-sanoj's-fabric-modpack-full
+IF ERRORLEVEL 12 GOTO 1.16-sanoj's-fabric-modpack-lite
+IF ERRORLEVEL 11 GOTO 1.16-sanoj's-forge-modpack-full
+IF ERRORLEVEL 10 GOTO 1.16-sanoj's-forge-modpack-lite
+IF ERRORLEVEL 9 GOTO 1.16-Client-Mods-Full-Shader
+IF ERRORLEVEL 8 GOTO 1.16-Client-Mods-Lite-Shader
+IF ERRORLEVEL 7 GOTO 1.16-Client-Only-Shader
+IF ERRORLEVEL 6 GOTO 1.16-Client-Mods-Full-FPS
+IF ERRORLEVEL 5 GOTO 1.16-Client-Mods-Lite-FPS
+IF ERRORLEVEL 4 GOTO 1.16-Client-Only-FPS
+IF ERRORLEVEL 3 GOTO 1.16-forge
+IF ERRORLEVEL 2 GOTO 1.16-fabric
+IF ERRORLEVEL 1 GOTO 1.16-optifine
 
 :tp
 CLS
@@ -145,16 +230,16 @@ IF ERRORLEVEL 2 GOTO dm
 IF ERRORLEVEL 1 GOTO k
 
 
-:1.16.5-optifine
+:1.16-optifine
 echo Instalation startet...
 C:
 cd %userprofile%\AppData\Roaming\.minecraft
 tar cf Backup.tar mods config
-curl --output 1.16.5-optifine.jar --url https://download.san0j.de/mods/1.16.5-optifine.jar
+curl --output 1.16-optifine.jar --url https://download.san0j.de/mods/1.16-optifine.jar
 echo Im n„chsten Schritt ”ffnet sich automatisch ein Fenster klicke dort nur auf "Install"!
 Pause
-java -jar 1.16.5-optifine.jar
-del /S /Q 1.16.5-optifine.jar
+java -jar 1.16-optifine.jar
+del /S /Q 1.16-optifine.jar
 echo Fertig!
 
 ECHO 1. Mod-Installer neustarten.
@@ -165,7 +250,7 @@ IF ERRORLEVEL 2 GOTO end
 IF ERRORLEVEL 1 GOTO start
 
 
-:1.16.5-fabric
+:1.16-fabric
 echo Instalation startet...
 C:
 cd %userprofile%\AppData\Roaming\.minecraft
@@ -183,7 +268,7 @@ IF ERRORLEVEL 2 GOTO end
 IF ERRORLEVEL 1 GOTO start
 
 
-:1.16.5-forge
+:1.16-forge
 echo Instalation startet...
 C:
 cd %userprofile%\AppData\Roaming\.minecraft
@@ -193,7 +278,7 @@ echo Im n„chsten Schritt ”ffnet sich automatisch ein Fenster klicke dort nur auf
 Pause
 java -jar forge.jar
 del /S /Q forge.jar
-del /S /Q 1.16.5-forge.jar.log
+del /S /Q forge.jar.log
 echo Fertig! 
 
 ECHO 1. Mod-Installer neustarten.
@@ -204,7 +289,7 @@ IF ERRORLEVEL 2 GOTO end
 IF ERRORLEVEL 1 GOTO start
 	
 
-:1.16.5-Client-Only-FPS
+:1.16-Client-Only-FPS
 echo Instalation startet...
 C:
 cd %userprofile%\AppData\Roaming\.minecraft
@@ -213,7 +298,7 @@ curl --output fabric.jar --url https://maven.fabricmc.net/net/fabricmc/fabric-in
 java -jar fabric.jar client -mcversion 1.16.5
 del /S /Q fabric.jar
 rmdir /S /Q mods
-curl --output mods.zip --url https://download.san0j.de/mods/1.16.5-Client-Only-FPS.zip
+curl --output mods.zip --url https://download.san0j.de/mods/1.16-Client-Only-FPS.zip
 tar -xf mods.zip
 del /S /Q mods.zip
 echo Fertig!
@@ -226,7 +311,7 @@ IF ERRORLEVEL 2 GOTO end
 IF ERRORLEVEL 1 GOTO start
 
 
-:1.16.5-Client-Mods-Lite-FPS
+:1.16-Client-Mods-Lite-FPS
 echo Instalation startet...
 C:
 cd %userprofile%\AppData\Roaming\.minecraft
@@ -235,7 +320,7 @@ curl --output fabric.jar --url https://maven.fabricmc.net/net/fabricmc/fabric-in
 java -jar fabric.jar client -mcversion 1.16.5
 del /S /Q fabric.jar
 rmdir /S /Q mods
-curl --output mods.zip --url https://download.san0j.de/mods/1.16.5-Client-Mods-Lite-FPS.zip
+curl --output mods.zip --url https://download.san0j.de/mods/1.16-Client-Mods-Lite-FPS.zip
 tar -xf mods.zip
 del /S /Q mods.zip
 echo Fertig!
@@ -248,7 +333,7 @@ IF ERRORLEVEL 2 GOTO end
 IF ERRORLEVEL 1 GOTO start
 
 
-:1.16.5-Client-Mods-Full-FPS
+:1.16-Client-Mods-Full-FPS
 echo Instalation startet...
 C:
 cd %userprofile%\AppData\Roaming\.minecraft
@@ -257,7 +342,7 @@ curl --output fabric.jar --url https://maven.fabricmc.net/net/fabricmc/fabric-in
 java -jar fabric.jar client -mcversion 1.16.5
 del /S /Q fabric.jar
 rmdir /S /Q mods
-curl --output mods.zip --url https://download.san0j.de/mods/1.16.5-Client-Mods-Full-FPS.zip
+curl --output mods.zip --url https://download.san0j.de/mods/1.16-Client-Mods-Full-FPS.zip
 tar -xf mods.zip
 del /S /Q mods.zip
 echo Fertig!
@@ -270,7 +355,7 @@ IF ERRORLEVEL 2 GOTO end
 IF ERRORLEVEL 1 GOTO start
 
 
-:1.16.5-Client-Only-Shader
+:1.16-Client-Only-Shader
 echo Instalation startet...
 C:
 cd %userprofile%\AppData\Roaming\.minecraft
@@ -279,7 +364,7 @@ curl --output fabric.jar --url https://maven.fabricmc.net/net/fabricmc/fabric-in
 java -jar fabric.jar client -mcversion 1.16.5
 del /S /Q fabric.jar
 rmdir /S /Q mods
-curl --output mods.zip --url https://download.san0j.de/mods/1.16.5-Client-Only-Shader.zip
+curl --output mods.zip --url https://download.san0j.de/mods/1.16-Client-Only-Shader.zip
 tar -xf mods.zip
 del /S /Q mods.zip
 echo Fertig!
@@ -292,7 +377,7 @@ IF ERRORLEVEL 2 GOTO end
 IF ERRORLEVEL 1 GOTO start
 
 
-:1.16.5-Client-Mods-Lite-Shader
+:1.16-Client-Mods-Lite-Shader
 echo Instalation startet...
 C:
 cd %userprofile%\AppData\Roaming\.minecraft
@@ -301,7 +386,7 @@ curl --output fabric.jar --url https://maven.fabricmc.net/net/fabricmc/fabric-in
 java -jar fabric.jar client -mcversion 1.16.5
 del /S /Q fabric.jar
 rmdir /S /Q mods
-curl --output mods.zip --url https://download.san0j.de/mods/1.16.5-Client-Mods-Lite-Shader.zip
+curl --output mods.zip --url https://download.san0j.de/mods/1.16-Client-Mods-Lite-Shader.zip
 tar -xf mods.zip
 del /S /Q mods.zip
 echo Fertig!
@@ -314,7 +399,7 @@ IF ERRORLEVEL 2 GOTO end
 IF ERRORLEVEL 1 GOTO start
 
 
-:1.16.5-Client-Mods-Full-Shader
+:1.16-Client-Mods-Full-Shader
 echo Instalation startet...
 C:
 cd %userprofile%\AppData\Roaming\.minecraft
@@ -323,7 +408,7 @@ curl --output fabric.jar --url https://maven.fabricmc.net/net/fabricmc/fabric-in
 java -jar fabric.jar client -mcversion 1.16.5
 del /S /Q fabric.jar
 rmdir /S /Q mods
-curl --output mods.zip --url https://download.san0j.de/mods/1.16.5-Client-Mods-Full-Shader.zip
+curl --output mods.zip --url https://download.san0j.de/mods/1.16-Client-Mods-Full-Shader.zip
 tar -xf mods.zip
 del /S /Q mods.zip
 echo Fertig!
@@ -336,7 +421,7 @@ IF ERRORLEVEL 2 GOTO end
 IF ERRORLEVEL 1 GOTO start
 
 
-:1.16.5-sanoj's-forge-modpack-lite
+:1.16-sanoj's-forge-modpack-lite
 echo Instalation startet...
 C:
 cd %userprofile%\AppData\Roaming\.minecraft
@@ -346,9 +431,9 @@ echo Im n„chsten Schritt ”ffnet sich automatisch ein Fenster klicke dort nur auf
 Pause
 java -jar forge.jar
 del /S /Q forge.jar
-del /S /Q 1.16.5-forge.jar.log
+del /S /Q forge.jar.log
 rmdir /S /Q mods
-curl --output mods.zip --url https://download.san0j.de/mods/1.16.5-Sanojs-Forge-Modpack-Lite.zip
+curl --output mods.zip --url https://download.san0j.de/mods/1.16-Sanojs-Forge-Modpack-Lite.zip
 tar -xf mods.zip
 del /S /Q mods.zip
 echo Fertig!
@@ -361,7 +446,7 @@ IF ERRORLEVEL 2 GOTO end
 IF ERRORLEVEL 1 GOTO start
 
 
-:1.16.5-sanoj's-forge-modpack-full
+:1.16-sanoj's-forge-modpack-full
 echo Instalation startet...
 C:
 cd %userprofile%\AppData\Roaming\.minecraft
@@ -371,9 +456,9 @@ echo Im n„chsten Schritt ”ffnet sich automatisch ein Fenster klicke dort nur auf
 Pause
 java -jar forge.jar
 del /S /Q forge.jar
-del /S /Q 1.16.5-forge.jar.log
+del /S /Q forge.jar.log
 rmdir /S /Q mods
-curl --output mods.zip --url https://download.san0j.de/mods/1.16.5-Sanojs-Forge-Modpack-Full.zip
+curl --output mods.zip --url https://download.san0j.de/mods/1.16-Sanojs-Forge-Modpack-Full.zip
 tar -xf mods.zip
 del /S /Q mods.zip
 echo Fertig!
@@ -386,7 +471,7 @@ IF ERRORLEVEL 2 GOTO end
 IF ERRORLEVEL 1 GOTO start
 
 
-:1.16.5-sanoj's-fabric-modpack-lite
+:1.16-sanoj's-fabric-modpack-lite
 echo Instalation startet...
 C:
 cd %userprofile%\AppData\Roaming\.minecraft
@@ -395,7 +480,7 @@ curl --output fabric.jar --url https://maven.fabricmc.net/net/fabricmc/fabric-in
 java -jar fabric.jar client -mcversion 1.16.5
 del /S /Q fabric.jar
 rmdir /S /Q mods
-curl --output mods.zip --url https://download.san0j.de/mods/1.16.5-Sanojs-Fabric-Modpack-Lite.zip
+curl --output mods.zip --url https://download.san0j.de/mods/1.16-Sanojs-Fabric-Modpack-Lite.zip
 tar -xf mods.zip
 del /S /Q mods.zip
 echo Fertig!
@@ -408,7 +493,7 @@ IF ERRORLEVEL 2 GOTO end
 IF ERRORLEVEL 1 GOTO start
 
 
-:1.16.5-sanoj's-fabric-modpack-full
+:1.16-sanoj's-fabric-modpack-full
 echo Instalation startet...
 C:
 cd %userprofile%\AppData\Roaming\.minecraft
@@ -417,7 +502,7 @@ curl --output fabric.jar --url https://maven.fabricmc.net/net/fabricmc/fabric-in
 java -jar fabric.jar client -mcversion 1.16.5
 del /S /Q fabric.jar
 rmdir /S /Q mods
-curl --output mods.zip --url https://download.san0j.de/mods/1.16.5-Sanojs-Fabric-Modpack-Full.zip
+curl --output mods.zip --url https://download.san0j.de/mods/1.16-Sanojs-Fabric-Modpack-Full.zip
 tar -xf mods.zip
 del /S /Q mods.zip
 echo Fertig!
@@ -430,9 +515,9 @@ IF ERRORLEVEL 2 GOTO end
 IF ERRORLEVEL 1 GOTO start
 
 
-:1.16.5-fos
+:1.16-fos
 echo Download startet...
-start https://download.san0j.de/mods/1.16.5-Forge-Server-Pack.zip
+start https://download.san0j.de/mods/1.16-Forge-Server-Pack.zip
 ECHO 1. Mod-Installer neustarten.
 ECHO 2. Installer beenden.
 CHOICE /C 12 /M "Auswahl: "
@@ -441,9 +526,9 @@ IF ERRORLEVEL 2 GOTO end
 IF ERRORLEVEL 1 GOTO start
 
 
-:1.16.5-fas
+:1.16-fas
 echo Download startet...
-start https://download.san0j.de/mods/1.16.5-Fabric-Server-Pack.zip
+start https://download.san0j.de/mods/1.16-Fabric-Server-Pack.zip
 ECHO 1. Mod-Installer neustarten.
 ECHO 2. Installer beenden.
 CHOICE /C 12 /M "Auswahl: "
