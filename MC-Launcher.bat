7f97f74f6aaab3fa20e58c72b6ed64d2ed06cdb7
2020Sanoj <75573284+2020Sanoj@users.noreply.github.com>
2021-06-20 17:35:05 +0200

    Add files via upload

--- MC-Launcher.bat
+++ MC-Launcher.bat
@@ -3,16 +3,16 @@ curl --output MC-Launcher.bat --url https://download.san0j.de/mods/MC-Launcher.b
 C:
 if exist %userprofile%\AppData\Roaming\.minecraft (
 if exist "C:\program files (x86)\Minecraft Launcher\MinecraftLauncher.exe" (
+
 C:
 cd "%userprofile%\AppData\Roaming\.minecraft\"
+tar cf Backup.tar mods config
+rmdir /S /Q mods
+
 ECHO.
 ECHO Mit welchem Mod-Profil Minecraft starten?
 ECHO.
 CHOICE /C 123456789 /M "Auswahl: "
-
-tar cf Backup.tar mods config
-rmdir /S /Q mods
-
 IF ERRORLEVEL 9 tar xf Profil-9.tar
 IF ERRORLEVEL 8 tar xf Profil-8.tar
 IF ERRORLEVEL 7 tar xf Profil-7.tar
