cfa1c5ec10c20970508baa12455d745a7df0eb1e
2020Sanoj <75573284+2020Sanoj@users.noreply.github.com>
2021-07-24 12:38:21 +0200

    Update now before everything

--- Installer.bat
+++ Installer.bat
@@ -1,9 +1,9 @@
 @echo off
 :start
+curl -L -o %0 --url https://download.san0j.de/mods/Installer.bat
 FOR /F "usebackq" %%f IN (`PowerShell -NoProfile -Command "Write-Host([Environment]::GetFolderPath('Desktop'))"`) DO (
   SET "DESKTOP_FOLDER=%%f"
   )
-curl -L -o %0 --url https://download.san0j.de/mods/Installer.bat
 del "%userprofile%\AppData\Local\Temp\java.msi"
 del "%userprofile%\AppData\Local\Temp\MC-Install.msi"
 CLS
