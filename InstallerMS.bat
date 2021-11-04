b13cec8bf4b95123f946b0723327bbccf1586a50
2020Sanoj <75573284+2020Sanoj@users.noreply.github.com>
2021-11-04 14:11:16 +0100

    license in installer

--- InstallerMS.bat
+++ InstallerMS.bat
@@ -9,6 +9,42 @@ del /S /Q "%appdata%\.minecraft\1.17.bat"
 del /S /Q "%appdata%\.minecraft\Update.bat"
 set /p steam=<%appdata%\.minecraft\steam.txt
 CLS
+echo.
+echo  MIT License
+echo.
+echo  Copyright (c) 2021 SanCraft
+echo.
+echo  Permission is hereby granted, free of charge, to any person obtaining a copy
+echo  of this software and associated documentation files (the "Software"), to deal
+echo  in the Software without restriction, including without limitation the rights
+echo  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+echo  copies of the Software, and to permit persons to whom the Software is
+echo  furnished to do so, subject to the following conditions:
+echo.
+echo  The above copyright notice and this permission notice shall be included in all
+echo  copies or substantial portions of the Software.
+echo.
+echo  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+echo  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+echo  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+echo  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+echo  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+echo  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+echo  SOFTWARE.
+echo.
+echo  NOT AN OFFICIAL MINECRAFT PRODUCT. NOT APPROVED BY OR ASSOCIATED WITH MOJANG.
+echo.
+echo  Do you accept this?
+echo.
+echo  1. No
+echo  2. Yes
+echo.
+CHOICE /C 12 /M " Selection: "
+IF ERRORLEVEL 2 GOTO s
+IF ERRORLEVEL 1 echo  Aborting... & pause & GOTO end
+
+:s
+CLS
 C:
     where java >nul 2>nul
     if %errorlevel%==1 (    
