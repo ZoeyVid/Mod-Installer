bd2b3e73d81bfa158f1b032cdaa04636c121e1fe
2020Sanoj <75573284+2020Sanoj@users.noreply.github.com>
2021-12-05 21:14:56 +0100

    Fix every File has the same content

--- Mod-Installer.bat
+++ Mod-Installer.bat
@@ -84,52 +84,64 @@ if not exist "%appdata%\.minecraft" (
     TASKKILL /T /F /IM Minecraft.exe
 	GOTO restart
 )
+
 C:
 cd %appdata%\.minecraft
 if exist *.tar (
-echo  converting Profiles and Backups from .tar to .zip...
+echo  Your Profiles and the Backup need to be converted from a .tar file to a .zip file
+echo  Start now?
 Pause
+echo  converting Profiles and Backups from .tar to .zip...
 if exist Backup.tar (
+rmdir /S /Q mods
 tar xf Backup.tar
 tar acf Backup.zip mods options.txt
 echo  Backup converted to a zip file!
 )
 if exist Profil-1.tar (
+rmdir /S /Q mods
 tar xf Profil-1.tar
 tar acf Profil-1.zip mods options.txt
 echo  Profil-1 converted to a zip file!
 )
 if exist Profil-2.tar (
+rmdir /S /Q mods
 tar xf Profil-2.tar
 tar acf Profil-2.zip mods options.txt
 echo  Profil-2 converted to a zip file!
 )
 if exist Profil-3.tar (
+rmdir /S /Q mods
 tar xf Profil-3.tar
 tar acf Profil-3.zip mods options.txt
 echo  Profil-3 converted to a zip file!
 )
 if exist Profil-4.tar (
+rmdir /S /Q mods
 tar xf Profil-4.tar
 tar acf Profil-4.zip mods options.txt
 echo  Profil-4 converted to a zip file!
 )
 if exist Profil-5.tar (
+rmdir /S /Q mods
 tar xf Profil-5.tar
 tar acf Profil-5.zip mods options.txt
 echo  Profil-5 converted to a zip file!
 )
 if exist Profil-6.tar (
+rmdir /S /Q mods
 tar xf Profil-6.tar
 tar acf Profil-6.zip mods options.txt
 echo  Profil-6 converted to a zip file!
 )
 if exist Profil-7.tar (
+rmdir /S /Q mods
 tar xf Profil-7.tar
 tar acf Profil-7.zip mods options.txt
 echo  Profil-7 converted to a zip file!
 )
 if exist Profil-8.tar (
+rmdir /S /Q mods
 tar xf Profil-8.tar
 tar acf Profil-8.zip mods options.txt
 echo  Profil-8 converted to a zip file!
