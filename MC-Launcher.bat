348ca92f76ab945b26c76120f3a42667968dd96c
2020Sanoj <75573284+2020Sanoj@users.noreply.github.com>
2021-12-06 14:37:56 +0100

    Version 5.1.3.1

--- MC-Launcher.bat
+++ MC-Launcher.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set ver=Version 5.1.3
+set ver=Version 5.1.3.1
 
 :start
 FOR /F "usebackq" %%f IN (`PowerShell -NoProfile -Command "Write-Host([Environment]::GetFolderPath('Desktop'))"`) DO (
