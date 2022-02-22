3ee577330aca8193b50b948387d970877cb9d7d6
Sanoj <75573284+2020Sanoj@users.noreply.github.com>
2022-02-22 19:31:56 +0100

    Update Installer.bat

--- Installer.bat
+++ Installer.bat
@@ -49,27 +49,11 @@ del /S /Q "%appdata%\.minecraft\forge.bat"
 
 CLS
 echo.
-echo  MIT License
+echo  Mozilla Public License 2.0
 echo.
 echo  Copyright (c) 2022 SanCraft
 echo.
-echo  Permission is hereby granted, free of charge, to any person obtaining a copy
-echo  of this software and associated documentation files (the "Software"), to deal
-echo  in the Software without restriction, including without limitation the rights
-echo  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-echo  copies of the Software, and to permit persons to whom the Software is
-echo  furnished to do so, subject to the following conditions:
-echo.
-echo  The above copyright notice and this permission notice shall be included in all
-echo  copies or substantial portions of the Software.
-echo.
-echo  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-echo  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-echo  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-echo  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-echo  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-echo  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-echo  SOFTWARE.
+echo  The latest Version of Mozilla Public License 2.0 can be found here: https://mozilla.org/en-US/MPL/2.0
 echo.
 echo  NOT AN OFFICIAL MINECRAFT PRODUCT. NOT APPROVED BY OR ASSOCIATED WITH MOJANG.
 echo.
