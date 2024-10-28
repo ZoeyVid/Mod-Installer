d14caa0c49ad33da19cfbdcf15adf7894a3fcff0
Zoey <zoey@z0ey.de>
2024-10-28 19:46:30 +0100

    Update 1.21.bat
    Signed-off-by: Zoey <zoey@z0ey.de>

--- 1.21.bat
+++ 1.21.bat
@@ -4,7 +4,7 @@ if exist %appdata%\.minecraft (
   cd %appdata%\.minecraft 
   curl --ssl-no-revoke -sL -o fabric.jar https://zvcdn.de/fa
   if exist fabric.jar (
-    java -jar fabric.jar client -mcversion 1.21.1 -loader 0.16.7
+    java -jar fabric.jar client -mcversion 1.21.3 -loader 0.16.8
     del /S /Q fabric.jar
   )
 )
