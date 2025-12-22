7cdf077d3ef7b9cf8149a4cdd033fb6c6e3aedeb
Zoey <zoey@z0ey.de>
2025-12-22 13:46:43 +0100

    Update 1.21.bat
    Signed-off-by: Zoey <zoey@z0ey.de>

--- 1.21.bat
+++ 1.21.bat
@@ -2,7 +2,7 @@
 C:
 if exist %appdata%\.minecraft (
   cd %appdata%\.minecraft 
-  curl --ssl-no-revoke -sL -o fabric.jar https://maven.fabricmc.net/net/fabricmc/fabric-installer/1.1.1/fabric-installer-1.1.1.jar
+  curl -sL -o fabric.jar https://maven.fabricmc.net/net/fabricmc/fabric-installer/1.1.1/fabric-installer-1.1.1.jar
   if exist fabric.jar (
     java -jar fabric.jar client -mcversion 1.21.10 -loader 0.18.3
     del /S /Q fabric.jar
